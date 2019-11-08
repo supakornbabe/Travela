f1801aceefc3c754135415694b6c9bd72ecd500d
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-08 17:25:52 +0700

    test

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ const Search = props => {
     return (
         <div className="hero_home version_1">
             <div className="content">
-                <h3>Travela! ACTION!!!!!!!!</h3>
+                <h3>Travela!</h3>
                 <p>The world is yours.</p>
                 <form>
                     <div id="custom-search-input">
