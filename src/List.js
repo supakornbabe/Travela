83eee0a7804704fa815460ddcbe710ab6073db82
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-11-08 16:29:40 +0700

    use react hooks for state and effect

--- src/List.js
+++ src/List.js
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class List extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hits: [],
-      total: 0,
-      ready: false,
-      query: props.query,
-      page: 1
+const List = props => {
+  const [hits, setHITS] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [ready, setReady] = useState(false);
+  const [query, setQuery] = useState(props.query);
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    handleSearchWithPage(1) //ComponentDidMount
+    return () => { console.log('unmount') } //componentWillUnmount
+  },
+    // eslint-disable-next-line
+    []// value in array will trigger componentDidUpdate
+  )
+
+  const handleInputChange = e => {
+    setQuery(e.target.value)
+  }
+  const numberList = () => {
+    let number = [];
+    if (total / 10 < 7) {
+      let round = Math.ceil(total / 10)
+      for (let i = 1; i <= round; i++) {
+        number.push(i)
+      }
     }
+    else if (currentPage > 4) {
+      number = [
+        currentPage - 3,
+        currentPage - 2,
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        currentPage + 2,
+        currentPage + 3
+      ]
+    }
+    else {
+      number = [1, 2, 3, 4, 5, 6, 7]
+    }
+    return number
   }
-  componentWillMount() {
-    this.handleSearchWithPage(1)
+  let trimByWord = (sentence) => {
+    let wordLength = 100
+    var result = sentence;
+    var resultArray = result.split(" ");
+    if (resultArray.length > wordLength) {
+      resultArray = resultArray.slice(0, wordLength);
+      result = resultArray.join(" ") + "...";
+    }
+    return result;
   }
-
-  handleInputChange = (e) => {
-    this.setState({
-      query: e.target.value
-    })
-  }
-
-  handleSearchWithPage = (n) => {
+  const handleSearchWithPage = n => {
     let body = {
       "from": 10 * (n - 1), "size": 10,
       "query": {
         "multi_match": {
-          "query": this.state.query,
+          "query": query,
           "fields": ["content", "title"],
           "operator": "and"
         }
@@ -40,191 +70,152 @@ class List extends React.Component {
         }
       }
     }
-    this.setState({
-      ready: false
-    })
+    setReady(false)
     axios.post('http://35.240.242.187:9200/nutch/_search', body).then(response => {
-      this.setState({
-        hits: response.data.hits.hits,
-        total: response.data.hits.total,
-        ready: true,
-        page: n
-      })
+      setHITS(response.data.hits.hits)
+      setTotal(response.data.hits.total)
+      setReady(true)
+      setCurrentPage(n)
     })
   }
+  return (
+    <>
+      {ready ?
+        <div>
 
-  render() {
-    const numberList = () => {
-      let number = [];
-      let total = this.state.total;
-      let page = this.state.page;
-      if (total / 10 < 7) {
-        let round = Math.ceil(total / 10)
-        for (let i = 1; i <= round; i++) {
-          number.push(i)
-        }
-      }
-      else if (page > 4) {
-        number = [
-          page - 3,
-          page - 2,
-          page - 1,
-          page,
-          page + 1,
-          page + 2,
-          page + 3
-        ]
-      }
-      else {
-        number = [1, 2, 3, 4, 5, 6, 7]
-      }
-      return number
-    }
+          <header className="static">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm">
+                  <div id="logo">
+                    <img
+                      src="img/Travela.png"
+                      data-retina="true"
+                      alt=""
+                      width="auto"
+                      height="40 px"
+                      onClick={props.onClickBack}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
 
-    let trimByWord = (sentence) => {
-      let wordLength = 100
-      var result = sentence;
-      var resultArray = result.split(" ");
-      if (resultArray.length > wordLength) {
-        resultArray = resultArray.slice(0, wordLength);
-        result = resultArray.join(" ") + "...";
-      }
-      return result;
-    }
-
-    return (
-      <>
-        {this.state.ready ?
-          <div>
-
-            <header className="static">
+          <main>
+            <div id="results">
               <div className="container">
                 <div className="row">
-                  <div className="col-sm">
-                    <div id="logo">
-                      <img
-                        src="img/Travela.png"
-                        data-retina="true"
-                        alt=""
-                        width="auto"
-                        height="40 px"
-                        onClick={this.props.onClickBack}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </header>
-
-            <main>
-              <div id="results">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="search_bar_list">
-                        <form>
-                          <input
-                            className="form-control"
-                            placeholder={this.state.query}
-                            onChange={this.handleInputChange}
-                          />
-                          <input
-                            type="submit"
-                            value="Search"
-                            onClick={() => this.handleSearchWithPage(1)}
-                          />
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="container margin_60_35" >
-                <div className="row">
                   <div className="col-md-12">
-                    {this.state.hits.map(p =>
-                      <div className="strip_list wow fadeIn">
-                        <small>
-                          {p._source.url}
-                        </small>
-                        <a href={p._source.url}>
-                          <h3>
-                            {p._source.title}
-                          </h3>
-                        </a>
-                        <p>
-                          {trimByWord(p._source.content)}
-                        </p>
-                      </div>
-                    )}
-                    <nav aria-label="" className="add_top_20">
-                      <ul className="pagination pagination-sm">
+                    <div className="search_bar_list">
+                      <form>
+                        <input
+                          className="form-control"
+                          placeholder={query}
+                          onChange={handleInputChange}
+                        />
+                        <input
+                          type="submit"
+                          value="Search"
+                          onClick={() => handleSearchWithPage(1)}
+                        />
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="container margin_60_35" >
+              <div className="row">
+                <div className="col-md-12">
+                  {hits.map(p =>
+                    <div className="strip_list wow fadeIn">
+                      <small>
+                        {p._source.url}
+                      </small>
+                      <a
+                        href={p._source.url}
+                       
+                      >
+                        <h3>
+                          {p._source.title}
+                        </h3>
+                      </a>
+                      <p>
+                        {trimByWord(p._source.content)}
+                      </p>
+                    </div>
+                  )}
+                  <nav aria-label="" className="add_top_20">
+                    <ul className="pagination pagination-sm">
+                      <li className={
+                        currentPage <= 1 ?
+                          "page-item disabled"
+                          :
+                          "page-item"}
+                      >
+                        <button
+                          className="page-link"
+                          onClick={() => {
+                            handleSearchWithPage(currentPage - 1);
+                          }}
+                        >
+                          Previous
+                          </button>
+                      </li>
+                      {numberList().map(n =>
                         <li className={
-                          this.state.page <= 1 ?
-                            "page-item disabled"
+                          currentPage === n ?
+                            "page-item active"
                             :
                             "page-item"}
                         >
                           <button
                             className="page-link"
                             onClick={() => {
-                              this.handleSearchWithPage(this.state.page - 1);
+                              handleSearchWithPage(n)
                             }}
                           >
-                            Previous
+                            {n}
                           </button>
                         </li>
-                        {numberList().map(n =>
-                          <li className={
-                            this.state.page === n ?
-                              "page-item active"
-                              :
-                              "page-item"}
-                          >
-                            <button
-                              className="page-link"
-                              onClick={() => {
-                                this.handleSearchWithPage(n)
-                              }}
-                            >
-                              {n}
-                            </button>
-                          </li>
-                        )}
-                        <li
-                          className={
-                            this.state.page > Math.floor(this.state.total / 10) ?
-                              "page-item disabled"
-                              :
-                              "page-item"
-                          }
+                      )}
+                      <li
+                        className={
+                          currentPage > Math.floor(total / 10) ?
+                            "page-item disabled"
+                            :
+                            "page-item"
+                        }
+                      >
+                        <button
+                          className="page-link"
+                          onClick={() => {
+                            handleSearchWithPage(currentPage + 1)
+                          }}
                         >
-                          <button
-                            className="page-link"
-                            onClick={() => {
-                              this.handleSearchWithPage(this.state.page + 1)
-                            }}
-                          >
-                            Next
+                          Next
                           </button>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
               </div>
-            </main>
+            </div>
+          </main>
 
-          </div>
-          :
-          <div id="preloader">
-            <div data-loader="circle-side" />
-          </div>
-        }
-      </>
-    );
-  }
+        </div>
+        :
+        <div id="preloader">
+          <div data-loader="circle-side" />
+        </div>
+      }
+    </>
+  )
+
 }
+
+
 
 export default List;
--- src/Search.js
+++ src/Search.js
@@ -1,31 +1,22 @@
 import React from 'react';
 
-class Search extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
-    }
-    render() {
-        return (
-            <div className="hero_home version_1">
-                <div className="content">
-                    <h3>Travela!</h3>
-                    <p>
-                        The world is yours.
-				    </p>
-                    <form>
-                        <div id="custom-search-input">
-                            <div className="input-group">
-                                <input className=" search-query" placeholder="Ex. City, Location ...." onChange={this.props.handleInputChange}></input>
-                                <input type="submit" className="btn_search" value="Search" onClick={this.props.onClickSearch} />
-                            </div>
+const Search = props => {
+    return (
+        <div className="hero_home version_1">
+            <div className="content">
+                <h3>Travela!</h3>
+                <p>The world is yours.</p>
+                <form>
+                    <div id="custom-search-input">
+                        <div className="input-group">
+                            <input className=" search-query" placeholder="Ex. City, Location ...." onChange={props.handleInputChange}></input>
+                            <input type="submit" className="btn_search" value="Search" onClick={props.onClickSearch} />
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Search;
