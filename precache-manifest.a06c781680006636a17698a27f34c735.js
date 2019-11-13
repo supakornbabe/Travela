5007ec1f20a09465f84bc83afd45e5f4e90e9b6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-13 09:54:42 +0000

    Deploy supakornbabe/Travela to github.com/supakornbabe/Travela.git:gh-pages

---
+++ precache-manifest.a06c781680006636a17698a27f34c735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0915d005a8ddf47662dfa1c0bb6d401a",
+    "url": "/Travela/index.html"
+  },
+  {
+    "revision": "5eb1deb6cb173e264360",
+    "url": "/Travela/static/css/main.9b7ca25d.chunk.css"
+  },
+  {
+    "revision": "a71627350896315da454",
+    "url": "/Travela/static/js/2.fec8cb6a.chunk.js"
+  },
+  {
+    "revision": "5eb1deb6cb173e264360",
+    "url": "/Travela/static/js/main.242c9848.chunk.js"
+  },
+  {
+    "revision": "cb0a03c0867192c0e947",
+    "url": "/Travela/static/js/runtime-main.f3f1b2b1.js"
+  }
+]);
